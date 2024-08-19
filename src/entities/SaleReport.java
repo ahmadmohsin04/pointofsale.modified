31773b93b54fe1004932012430628d23d704cb8f
Ahmad Mohsin <business.ahmadmohsin@gmail.com>
2024-08-19 19:58:56 +0500

    sales class

--- src/entities/SaleReport.java
+++ src/entities/SaleReport.java
@@ -1,7 +1,22 @@
 
 package entities;
-
+import java.util.ArrayList;
 public class SaleReport {
-    
-    
+
+    public static void generateMonthlyReport(ArrayList<Transaction> transactions, int month, int year) {
+        double totalRevenue = 0;
+        int transactionCount = 0;
+
+        for (Transaction transaction : transactions) {
+            if (transaction.getDate().getMonthValue() == month && transaction.getDate().getYear() == year) {
+                totalRevenue += transaction.getTotalAmount();
+                transactionCount++;
+            }
+        }
+
+        System.out.println("Monthly Sales Report for " + month + "/" + year);
+        System.out.println("Total Revenue: RS." + totalRevenue);
+        System.out.println("Number of Transactions: " + transactionCount);
+    }
 }
+
